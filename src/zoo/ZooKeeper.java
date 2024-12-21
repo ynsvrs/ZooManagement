f56f50817e9f20e193a0b5c57e47b97731e6ff9f
ysnvrs <saniyadair@gmail.com>
2024-12-22 01:23:16 +0600

    zoomanagement

--- src/zoo/ZooKeeper.java
+++ src/zoo/ZooKeeper.java
@@ -9,15 +9,18 @@ public class ZooKeeper {
         this.id = id;
     }
     public String getName() {
+
         return name;
     }
     public void setName(String name) {
         this.name = name;
     }
     public int getId() {
+
         return id;
     }
     public void setId(int id) {
+
         this.id = id;
     }
     public void display() {
